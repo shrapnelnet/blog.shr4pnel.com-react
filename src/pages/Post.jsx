4befa3c3ab35fd0702e8853cdd1fe7aa3a4d8f58
tyler :3 <tyler@shrapnelnet.co.uk>
2024-01-22 18:43:41 +0000

    implement caching at individual post level

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import "../style/Post.css"
 import Markdown from "react-markdown"
 import matter from "gray-matter"
 import rehypeRaw from "rehype-raw"
 import emoji from "remark-emoji"
 import { Link } from "react-router-dom"
-export default function Post() {
-    const [title, setTitle] = useState("")
-    const [date, setDate] = useState("")
-    const [content, setContent] = useState("")
+import { useQuery } from "@tanstack/react-query"
 
-    useEffect(() => {
-        const headers = new Headers()
-        headers.set("Accept", "text/markdown")
-        // todo hacky change l8r pls
-        const name = document.location.pathname.split("/")[2]
-        fetch(`/posts/${name}.md`, {headers})
-            .then((res) => res.text())
-            .then((res) => matter(res))
-            .then((post) => {
-                setTitle(post.data.title)
-                setDate(post.data.date)
-                setContent(post.content)
-            })
-    }, [])
+export default function Post() {
+    const name = document.location.pathname.split("/")[2]
+
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: [name],
+        queryFn: () => {
+            const headers = new Headers()
+            headers.set("Accept", "text/markdown")
+            return fetch(`/posts/${name}.md`)
+                .then((res) => res.text())
+                .then((res) => matter(res))
+        }
+    })
+
+    if (isPending) {
+        return <></>
+    }
+
+    if (isError) {
+        return <h4>{error.message}</h4>
+    }
+
     return (
         <React.Fragment>
             <header>
@@ -36,10 +41,10 @@ export default function Post() {
                     <article>
                         <div id="content" className="use-ibm">
                             <div id="title">
-                                <h1>{title}</h1>
+                                <h1>{data.data.title}</h1>
                             </div>
                             <div id="date">
-                                <p>{new Date(date).toUTCString()}</p>
+                                <p>{new Date(data.data.date).toUTCString()}</p>
                             </div>
                             <sub>by tyler, yes, the tyler!</sub>
                             <div className="flex-newline"></div>
@@ -47,7 +52,7 @@ export default function Post() {
                             <Markdown disallowedElements={["script", "meta", "iframe", "object", "embed"]}
                                       rehypePlugins={[rehypeRaw]}
                                       remarkPlugins={[emoji]}>
-                                {content}
+                                {data.content}
                             </Markdown>
                         </div>
                     </article>
