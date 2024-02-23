560d675910544e2f130bc7a936108aa6dde9ccda
tyler :3 <tyler@shrapnelnet.co.uk>
2024-02-23 18:56:46 +0000

    fix img width

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,31 +5,27 @@ export default function Socials() {
         <React.Fragment>
             <div>
                 <div className="fake-logo"></div>
-                <a href="/links">My webrings!</a>
-            </div>
-            <div>
-                <div className="fake-logo"></div>
                 <a href="mailto:admin@shr4pnel.com">My email</a>
             </div>
             <div>
                 <img
                     src="/graphics/github-mark.svg"
                     alt="github logo"
-                    width="32px"
-                    height="32px"
+                    width="32"
+                    height="32"
                 />
                 <a href="https://github.com/shrapnelnet/" target="_blank" rel="noreferrer">
                     github/shrapnelnet
                 </a>
             </div>
             <div>
-                <img src="/graphics/mastodon.svg" alt="mastodon logo" width="32px" height="32px"/>
+                <img src="/graphics/mastodon.svg" alt="mastodon logo" width="32" height="32"/>
                 <a href="https://blobfox.coffee/@tyler" target="_blank" rel="noreferrer">
                     @tyler@blobfox.coffee
                 </a>
             </div>
             <div>
-                <img src="/graphics/rss_optimised.png" alt="rss logo" width="32px" height="32px"/>
+                <img src="/graphics/rss_optimised.png" alt="rss logo" width="32" height="32" />
                 <a href="/rss.xml">RSS feed</a>
             </div>
         </React.Fragment>
